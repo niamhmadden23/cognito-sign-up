8c6923062f866001f033b2d9268da2c6ee59128a
Niamh Madden <niamhmadden15@gmail.com>
2022-03-30 09:15:33 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsConfig from './src/aws-exports';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
 }
+
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/aws-exports.js
@@ -0,0 +1,10 @@
+
+
+const awsConfig = {
+    identityPoolId: 'eu-west-2_mathvHOc3',
+    region: 'eu-west-2',
+    userPoolId: 'eu-west-2_mathvHOc3',
+    userPoolWebClientId: '6s7u9d3a5uuacrunhpib9pnqtr'
+  }
+  
+  export default awsConfig;
